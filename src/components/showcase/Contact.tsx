c60793af7cc8e6c60723ab803a634d23bc3097d7
Jermaine Munene <fustrumstudios@gmail.com>
2025-12-10 15:44:43 +0300

    Update contact API URL in Contact.tsx

--- src/components/showcase/Contact.tsx
+++ src/components/showcase/Contact.tsx
@@ -57,7 +57,7 @@ const Contact: React.FC<ContactProps> = (props) => {
         try {
             setIsLoading(true);
             const res = await fetch(
-                'https://api.jermainemunene.site/api/contact',
+                'https://api.jermainemunene.online/api/contact',
                 {
                     method: 'POST',
                     headers: {
